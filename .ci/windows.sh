42bb9335d2448d6df65b6d1d10a0cf0a02f50513
Evilmass <21218296+Evilmass@users.noreply.github.com>
2025-08-01 22:36:43 +0800

    fix msys2

--- .ci/windows.sh
+++ .ci/windows.sh
@@ -2,11 +2,12 @@
 
 mkdir build && cd build
 cmake .. -G Ninja \
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_C_COMPILER_LAUNCHER=ccache \
     -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
     -DENABLE_QT_TRANSLATION=ON \
-    -DUSE_DISCORD_PRESENCE=ON
+    -DUSE_DISCORD_PRESENCE=OFF
 ninja
 ninja bundle
 strip -s bundle/*.exe
